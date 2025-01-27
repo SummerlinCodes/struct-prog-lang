d9bebe4b52fac66910fa7a0646dce75d75cab5e0
SummerlinCodes <summerlinwork30@gmail.com>
2025-01-27 23:35:32 +0000

    Jan. 27th partial notes

--- My Version Notes/Topic-02-PMDAS/parser.py
+++ My Version Notes/Topic-02-PMDAS/parser.py
@@ -1,2 +1,12 @@
 ## Jan 27th
 
+## AST = Abstract Syntax Tree
+Accept a string of tokens, return an AST expressed as stack of dictionaries
+"""
+"""
+    simple_expression = number | "()" expression ")" | "-" simple_expression
+    factor = simple_expression
+    term = factor { "*"|"/" factor }
+    arithmetic_expression = term { "+"|"-" term } # means plus or minus term
+    expression = arithmetic_expression
+"""
